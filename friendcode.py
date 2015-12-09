1aa0db7173216d9412803546d7f95a87bb7e5ce6
typoguy <typoguy@users.noreply.github.com>
2015-12-09 16:41:10 -0600

    Converted input to lowercase for the case sensitivity issue; added 'user does not exist' responses.

--- friendcode.py
+++ friendcode.py
@@ -7,21 +7,21 @@ try:
 except Exception as e:
     raise e
 
-# Usage:
-#
-#    .friendcode or .fc will work:
+# Usage: .friendcode or .fc will work.
 #    .fc Displays your friendcode if set.
 #    .fc [code] Sets your friendcode
 #    .fc [nick] Displays the friendcode for another person.
 #
-#    .friendcodedelete or .fcdelete or .fcdel:
-#    .fcdel Removes your friendcode.
+#    .friendcodedelete or .fcdelete or .fcdel
+#    .fcdel Removes your friendcode. 
 
 r = re.compile('^\d{4}-\d{4}-\d{4}$')
 
 @hook.command('fc')
 @hook.command
 def friendcode(inp, nick=''):
+    nick = nick.lower()
+    inp = inp.lower()
     if inp:
         target = table.find_one(nickname=inp)
         if r.match(inp):
@@ -30,15 +30,19 @@ def friendcode(inp, nick=''):
         else:
             if target:
                 return "{}'s friendcode is {}".format(inp, target['code'])
+            else:
+                return "{} does not have a friendcode".format(inp)
     else:
         target = table.find_one(nickname=nick)
         if target:
             return target['code']
-
+        else:
+            return "You do not have a friendcode"
 @hook.command('fcdel')
 @hook.command('fcdelete')
 @hook.command
 def friendcodedelete(inp, nick=''):
+    nick = nick.lower()
     target = table.find_one(nickname=nick)
     if target:
         table.delete(nickname=nick)
